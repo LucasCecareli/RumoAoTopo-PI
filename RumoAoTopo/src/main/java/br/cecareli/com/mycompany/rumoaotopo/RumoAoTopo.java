c861def4cb4c374f7e265366a35ebae883f9a8ad
Lucas Cecareli <25.00342-7@NOTE-35899.maua.br>
2025-05-14 09:54:15 -0300

    TESTE

---
+++ RumoAoTopo/src/main/java/br/cecareli/com/mycompany/rumoaotopo/RumoAoTopo.java
@@ -0,0 +1,11 @@
+
+
+package br.cecareli.com.mycompany.rumoaotopo;
+
+
+public class RumoAoTopo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
