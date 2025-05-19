7550811cbfb8e9a42de2a9191c0d9a2c5ea4d953
Lucas Cecareli <25.00342-7@NOTE-35899.maua.br>
2025-05-19 11:53:34 -0300

    --criando a classe usuario

---
+++ RumoAoTopo/src/main/java/br/cecareli/com/mycompany/rumoaotopo/Usuario.java
@@ -0,0 +1,13 @@
+
+package br.cecareli.com.mycompany.rumoaotopo;
+
+import lombok.*;
+@Getter
+@Setter
+@AllArgsConstructor
+public class Usuario {
+    private String login, senha ;
+
+    
+    
+}
